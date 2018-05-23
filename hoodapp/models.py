ba6a524a51f1fbd1b218c9a80d2be9bf783fa400
ppolle <peter.m.polle@gmail.com>
2018-05-23 14:55:17 +0300

    this commit will create a Business model

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Business(models.Model):
+	name = models.CharField(max_length = 300)
+	description = models.TextField()
+	email_address = models.EmailField()
+	
+	def __str__(self):
+		return self.name
+
+
